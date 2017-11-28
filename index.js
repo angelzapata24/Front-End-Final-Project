4ba4ed2aba3ead44dca4631573ca711b4a32c239
Angel Zapata <azapata@basecampcodingacademy.org>
2017-11-28 10:04:08 -0600

    I change my img source from downloads to just putting the link from where I got the picture from

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var PAGE_DATA = {
             Price: 210,
             size: '8.5',
             quantity: 1,
-            img: '../../../../../Downloads/toro.jpg',
+            img:
+                'https://c1.staticflickr.com/4/3869/15099599532_58f9973803_b.jpg',
             seller: "AZ's Sneakers"
         },
         {
@@ -13,7 +14,8 @@ var PAGE_DATA = {
             Price: 210,
             size: '9',
             quantity: 2,
-            img: '../../../../../Downloads/gamma.png',
+            img:
+                'http://picture-cdn.wheretoget.it/3kj06b-l-610x610-shoes-jordan%20gamma%20blue%2011s-black-air%20jordans-jordans.jpg',
             seller: "AZ's Sneakers"
         },
         {
@@ -21,7 +23,8 @@ var PAGE_DATA = {
             Price: 190,
             size: '8',
             quantity: 3,
-            img: '../../../../../Downloads/playoff.png',
+            img:
+                'https://i2.wp.com/kickssavage.com/wp-content/uploads/2017/04/jordan-13-playoff-2017-on-feet.jpg?fit=564%2C454',
             seller: "AZ's Sneakers"
         },
         {
@@ -29,7 +32,8 @@ var PAGE_DATA = {
             Price: 300,
             size: '11',
             quantity: 1,
-            img: '../../../../../Downloads/humanrace.jpg',
+            img:
+                'https://cdn5.kicksonfire.com/wp-content/uploads/2016/07/PHARRELL-WILLIAMS-X-ADIDAS-NMD-3.jpg?x76107',
             seller: "AZ's Sneakers"
         },
         {
@@ -37,7 +41,8 @@ var PAGE_DATA = {
             Price: 205,
             size: '9.5',
             quantity: 4,
-            img: '../../../../../Downloads/barons.jpg',
+            img:
+                'https://i.pinimg.com/736x/d8/e7/32/d8e732b7a19366b78489a44cda363f57--jordan-swag-jordan-.jpg',
             seller: "AZ's Sneakers"
         },
         {
@@ -45,7 +50,8 @@ var PAGE_DATA = {
             Price: 200,
             size: '9',
             quantity: 2,
-            img: '../../../../../Downloads/yezzy.jpg',
+            img:
+                'https://ssli.ulximg.com/image/740x493/cover/1508946603_15b5dd32bf40ab4a93d8ba1d699b9f6c.jpg/5b75a38b9fb5b0397152c32ecc374430/1508946600_4777a9007701ad67052f745744c4808c.jpg',
             seller: "AZ's Sneakers"
         },
         {
@@ -53,7 +59,8 @@ var PAGE_DATA = {
             Price: 170,
             size: '11',
             quantity: 6,
-            img: '../../../../../Downloads/japan_nmd.jpg',
+            img:
+                'https://www.43einhalb.com/files/image/id/123295/w/2000/h/1200/n/adidas-nmd_r1-pk-triple-white-japan-bz0221-mood-1.jpg',
             seller: "AZ's Sneakers"
         },
         {
@@ -61,7 +68,8 @@ var PAGE_DATA = {
             Price: 225,
             size: '9.5',
             quantity: 8,
-            img: '../../../../../Downloads/winlike82.jpg',
+            img:
+                'https://sneakernews.com/wp-content/uploads/2017/10/air-jordan-11-win-like-82.jpg',
             seller: "AZ's Sneakers"
         },
         {
@@ -69,7 +77,8 @@ var PAGE_DATA = {
             Price: 170,
             size: '10',
             quantity: 3,
-            img: '../../../../../Downloads/bred.jpg',
+            img:
+                'http://www.modern-notoriety.com/wp-content/uploads/2016/07/air-jordan-1-bred_10.jpg',
             seller: "AZ's Sneakers"
         }
     ]
